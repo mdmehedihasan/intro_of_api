6922e75cc60c35aad9bbba85f373fe098d602bbb
mehedi hasan <mehedicsebu@gmail.com>
2022-02-24 00:09:33 +0600

    api updated

--- js/api.js
+++ js/api.js
@@ -2,13 +2,13 @@
 function loadData() {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then(response => response.json())
-        .then(data => displayUser(data))
+        .then(data => console.log(data))
 }
 //load user
 function loadUser() {
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(user => console.log(user))
+        .then(user => displayUser(user))
 }
 //post call
 function postUser() {
@@ -16,6 +16,12 @@ function postUser() {
         .then(res => res.json())
         .then(data => console.log(data));
 }
-function displayUser(data) {
-    console.log(data);
+function displayUser(user) {
+    const ul = document.getElementById('users');
+    for (const users of user) {
+        console.log(users.name);
+        const li = document.createElement('li');
+        li.innerText = `name: ${users.name} | email: ${users.email}`;
+        ul.appendChild(li);
+    }
 }
